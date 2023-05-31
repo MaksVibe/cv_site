fb803be7a9e555b45dfcadafdfd28ac4b726d53c
Maksym <90103151+MaksVibe@users.noreply.github.com>
2023-05-31 19:43:18 +0300

    fix: header position

--- components/Header/Header.styles.ts
+++ components/Header/Header.styles.ts
@@ -4,7 +4,9 @@ export const Wrapper = styled.header`
     display: flex;
     justify-content: space-between;
     align-items: center;
+
     max-width: 1140px;
+    margin: 0 auto;
     padding: 2rem 1rem;
     background: linear-gradient(0deg, rgba(1,10,0,0) 0%, rgba(1,10,0,0.9) 40%, rgba(1,10,0,1) 80%);
 `;
